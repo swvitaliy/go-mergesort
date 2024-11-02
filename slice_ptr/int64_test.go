db86be78e1e1d7a5ab3f4d8b483f90b82f2af463
Vitaliy <sw.vitaliy@gmail.com>
2024-11-03 00:03:23 +0400

    added slice_ptr benchmarks

--- slice_ptr/int64_test.go
+++ slice_ptr/int64_test.go
@@ -1,13 +1,36 @@
 package slice_ptr
 
 import (
+	"fmt"
 	"testing"
 )
 
+var table = []struct {
+	input int
+}{
+	{input: 1_000},
+	{input: 10_000},
+	{input: 100_000},
+	{input: 1_000_000},
+	//{input: 10_000_000},
+}
+
 func BenchmarkInt64ValuesV1(b *testing.B) {
-	Int64ValuesV1(b.N)
+	for _, v := range table {
+		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				Int64ValuesV1(v.input)
+			}
+		})
+	}
 }
 
 func BenchmarkInt64ValuesV2(b *testing.B) {
-	Int64ValuesV2(b.N)
+	for _, v := range table {
+		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				Int64ValuesV2(v.input)
+			}
+		})
+	}
 }
--- slice_ptr/props8_test.go
+++ slice_ptr/props8_test.go
@@ -1,13 +1,26 @@
 package slice_ptr
 
 import (
+	"fmt"
 	"testing"
 )
 
 func BenchmarkProps8PointersV1(b *testing.B) {
-	Props8PointersV1(b.N)
+	for _, v := range table {
+		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				Props8PointersV1(v.input)
+			}
+		})
+	}
 }
 
 func BenchmarkProps8PointersV2(b *testing.B) {
-	Props8PointersV2(b.N)
+	for _, v := range table {
+		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				Props8PointersV2(v.input)
+			}
+		})
+	}
 }
