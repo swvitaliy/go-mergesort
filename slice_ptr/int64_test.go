516beb515b1fca41ac60ffe56a7c1171da7b1c1d
Vitaliy <sw.vitaliy@gmail.com>
2024-12-16 21:20:24 +0400

    slice_ptr w\o gc

--- slice_ptr/int64_test.go
+++ slice_ptr/int64_test.go
@@ -2,6 +2,8 @@ package slice_ptr
 
 import (
 	"fmt"
+	"math"
+	"runtime/debug"
 	"testing"
 )
 
@@ -15,22 +17,42 @@ var table = []struct {
 	//{input: 10_000_000},
 }
 
+const gc = "disable"
+
+func gcDisable(fn func()) {
+	if gc == "enable" {
+		fn()
+		return
+	}
+
+	gcpercent := debug.SetGCPercent(-1)
+	memlimit := debug.SetMemoryLimit(math.MaxInt64)
+	fn()
+	debug.SetGCPercent(gcpercent)
+	debug.SetMemoryLimit(memlimit)
+
+}
+
 func BenchmarkInt64ValuesV1(b *testing.B) {
 	for _, v := range table {
-		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				Int64ValuesV1(v.input)
-			}
+		gcDisable(func() {
+			b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					Int64ValuesV1(v.input)
+				}
+			})
 		})
 	}
 }
 
 func BenchmarkInt64ValuesV2(b *testing.B) {
 	for _, v := range table {
-		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				Int64ValuesV2(v.input)
-			}
+		gcDisable(func() {
+			b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					Int64ValuesV2(v.input)
+				}
+			})
 		})
 	}
 }
--- slice_ptr/props8_test.go
+++ slice_ptr/props8_test.go
@@ -7,20 +7,24 @@ import (
 
 func BenchmarkProps8PointersV1(b *testing.B) {
 	for _, v := range table {
-		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				Props8PointersV1(v.input)
-			}
+		gcDisable(func() {
+			b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					Props8PointersV1(v.input)
+				}
+			})
 		})
 	}
 }
 
 func BenchmarkProps8PointersV2(b *testing.B) {
 	for _, v := range table {
-		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				Props8PointersV2(v.input)
-			}
+		gcDisable(func() {
+			b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					Props8PointersV2(v.input)
+				}
+			})
 		})
 	}
 }
