60753597425129d90026c4cae66bb9e479e4ebff
Vitaliy <sw.vitaliy@gmail.com>
2024-12-16 21:07:22 +0400

    weak sync maps bench

--- weak_cache/tests.go
+++ weak_cache/tests.go
@@ -1,5 +1,9 @@
 package weak_cache
 
+import "runtime"
+
+const gc = false
+
 type ICache[K comparable, V any] interface {
 	Get(key K) (*V, bool)
 	Set(key K, value V)
@@ -11,6 +15,9 @@ func readWrite1(c ICache[int, int], n, m int) {
 	}
 	for i := 0; i < m; i++ {
 		c.Get(i)
+		if gc && i%1000 == 0 {
+			runtime.GC()
+		}
 	}
 }
 
@@ -18,5 +25,8 @@ func readWrite2(c ICache[int, int], n int) {
 	for i := 0; i < n; i++ {
 		c.Set(i, i)
 		c.Get(i)
+		if gc && i%1000 == 0 {
+			runtime.GC()
+		}
 	}
 }
